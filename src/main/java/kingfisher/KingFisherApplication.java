ae182f8bdaebef6f8356ecdf46f622d0a730ef57
xiuchengquek <biobirdman@gmail.com>
2015-05-27 01:43:27 +1000

    Sql Version

--- src/main/java/kingfisher/KingFisherApplication.java
+++ src/main/java/kingfisher/KingFisherApplication.java
@@ -2,10 +2,20 @@ package kingfisher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 
 @SpringBootApplication
-public class KingFisherApplication {
+public class KingFisherApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(KingFisherApplication.class);
+
+
+
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KingFisherApplication.class, args);
