700c0e805079ee1407fa382a8b4adbe7d405af0f
xiuchengquek <biobirdman@gmail.com>
2015-06-25 21:48:20 +1000

    added bower for dependencies, changed js location in templates, but need to change css location and jasmine tests

--- src/main/resources/static/js/kingfisherApp/components/controlPanel/controlPanelController.js
+++ src/main/resources/static/js/kingfisherApp/components/controlPanel/controlPanelController.js
@@ -9,18 +9,12 @@ angular.module('kingFisherApp')
 
         $scope.data = {};
         $scope.results = {};
-
         $scope.update = function() {
             dataLoader.loadAndValidate($scope.data.maf, $scope.data.clinical);
             dataLoader.doCall().then(function(results){
-
                 var results = { newick : results.data, vafMap :dataLoader.getVafMap(), timePoint : dataLoader.getTimePoint()  }
                 $scope.results = results;
                 $('#headingTwoLink').trigger('click')
-
-
-
-
             });
         };
 
@@ -28,21 +22,16 @@ angular.module('kingFisherApp')
             var mockClinical = ["Tumor_Sample_Barcode\tBiopsy_Time\tTreatment",
                             "sample-A01\t1\tTreatmentA",
                             "sample-B01\t2\tTreatmentB",
-                            "sample-C01\t3\tTreatmentC"];
+                            "sample-C01\t3\tTreatmentC"
+            ];
 
             var mockClinical = mockClinical.join("\n");
-
             $http.get('js/kingfisherApp/components/controlPanel/example.tsv').then(function(results){
                 $scope.data.maf = results.data;
                 $scope.data.clinical = mockClinical;
             })
         };
 });
-
-
-
-
-
 /**
  * Created by xiuchengquek on 14/06/15.
  */
